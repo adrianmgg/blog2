4571df9a6ebe3fc781d0d4284353cb010bcfa723
amgg <16143061+adrianmgg@users.noreply.github.com>
2026-05-14 18:10:43 -0700

    stuff

--- flake.nix
+++ flake.nix
@@ -3,21 +3,52 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
   };
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
+        inputs.process-compose-flake.flakeModule
       ];
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux"];
       perSystem = {
         self',
         pkgs,
+        lib,
         ...
       }: let
         python = pkgs.python3;
         pythonPackages = pkgs.python3Packages;
+        pelican = pythonPackages.pelican;
       in {
-        packages.default = pkgs.hello;
+        # `nix run` -> watch w/ live server
+        apps.default = {
+          type = "app";
+          program = self'.packages.watch;
+        };
+        # `nix build` -> builds the site
+        packages.default = self'.packages.blog;
+
+        packages.blog = pkgs.stdenv.mkDerivation {
+          src = ./blog;
+          pname = "amgg-blog";
+          version = "1";
+          buildInputs = [pelican];
+          buildPhase = ''
+            pelican --output $out
+          '';
+        };
+
+        process-compose.watch = {
+          settings.processes = {
+            site-builder.command = "cd blog && ${lib.getExe' pelican "pelican"} --autoreload --delete-output-directory --ignore-cache .";
+            live-server = {
+              command = "${lib.getExe pkgs.live-server} --index --open / ./blog/output";
+              depends_on.site-builder.condition = "process_started";
+            };
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [pythonPackages.pelican];
         };
