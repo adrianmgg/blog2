8f7a96c13e6fb6d68f4edba8fef2462ed6a13e9f
amgg <16143061+adrianmgg@users.noreply.github.com>
2026-05-14 17:26:48 -0700

    stuff

---
+++ blog/content/hello-world.rst
@@ -0,0 +1,11 @@
+Hello World
+###########
+
+:date: 2026-05-14
+:status: published
+
+hello world
+
+
+
+
--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -15,21 +15,30 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Github", "https://github.com/adrianmgg/"),
+    ("Bluesky", "https://bsky.app/profile/amgg.gg"),
+    ("Mastodon", "https://hachyderm.io/@amgg"),
+    ("Tumblr", "https://image-wizard.tumblr.com/"),
 )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+# write original files to output too
+# OUTPUT_SOURCES = True
+
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
